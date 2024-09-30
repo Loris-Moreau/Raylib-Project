1d1b6e2bf4eb7acc22173129f2b49a600b1d570b
Loris M. <80474693+Loris-Moreau@users.noreply.github.com>
2024-09-30 15:17:50 +0200

    [fix] window

--- Window.cpp
+++ Window.cpp
@@ -1,3 +1,4 @@
+#include <complex.h>
 using namespace std;
 
 #include <iostream>
@@ -13,117 +14,54 @@ int main()
     constexpr int screenHeight = 960;
     InitWindow(screenWidth, screenHeight, "Intermediate AI");
 
+    int flockAmount = 3;
     // Create a flock of boids
-    std::vector<Boids> flock =
-    {
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1),
-        Boids(100, 100, 2, 2),
-        Boids(200, 200, -2, -2),
-        Boids(150, 150, 1.5, -1.5),
-        Boids(300, 300, -1, 1)
-    };
-
+    
+   
+        std::vector<Boids> flock =
+        {
+            Boids(100, 100, 2, 2),
+            Boids(200, 200, -2, -2),
+            Boids(150, 150, 1.5, -1.5),
+            Boids(300, 300, -1, 1),
+            Boids(100, 100, 2, 2),
+            Boids(200, 200, -2, -2),
+            Boids(150, 150, 1.5, -1.5),
+            Boids(300, 300, -1, 1),
+            Boids(100, 100, 2, 2),
+            Boids(200, 200, -2, -2),
+            Boids(150, 150, 1.5, -1.5),
+            Boids(300, 300, -1, 1),
+            Boids(100, 100, 2, 2),
+            Boids(200, 200, -2, -2),
+            Boids(150, 150, 1.5, -1.5),
+            Boids(300, 300, -1, 1),
+            Boids(100, 100, 2, 2),
+            Boids(200, 200, -2, -2),
+            Boids(150, 150, 1.5, -1.5),
+            Boids(300, 300, -1, 1),
+            Boids(100, 100, 2, 2),
+            Boids(200, 200, -2, -2),
+            Boids(150, 150, 1.5, -1.5),
+            Boids(300, 300, -1, 1),
+            Boids(100, 100, 2, 2),
+            Boids(200, 200, -2, -2),
+            Boids(150, 150, 1.5, -1.5),
+            Boids(300, 300, -1, 1),
+            Boids(100, 100, 2, 2)
+        };
+    
     // Create some obstacles
     std::vector<Obstacle> obstacles =
     {
-        Obstacle(45, 200, {300, 450}),
-        Obstacle(40, 40, {500, 200}),
-        Obstacle(110, 15, {600, 600})
+        Obstacle(60, 250, {125, 500}),
+        Obstacle(65, 65, {500, 75}),
+        Obstacle(110, 150, {850, 750}),
+        Obstacle(75, 75, {500, 500})
     };
 
     // Define the simulation parameters
-    float minDistance = 30.0f;
+    float minDistance = 25.0f;
     float alignmentFactor = 0.45f;
     float cohesionFactor = 0.45f;
     float maxSpeed = 7.4f;
@@ -140,7 +78,7 @@ int main()
 
         // Draw everything
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(LIGHTGRAY);
 
         // Draw boids
         for (const auto& boid : flock)
